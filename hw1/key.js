f68e78bf939fe79e8870160720a896aaba581f9c
李嘉晟 <johnson50701@gmail.com>
2018-10-30 22:39:53 +0800

    Add Answer.txt and modifiy key.js

--- hw1/key.js
+++ hw1/key.js
@@ -1,33 +1,25 @@
-// npm-library
 const Wallet = require('ethereumjs-wallet');
 const keccak256 = require('js-sha3').keccak256;
-
 const fs = require('fs');
 
-// keypair
+// generate wallet.
 const wallet = Wallet.generate();
  
-// privKey
+// 1
 const privKey = wallet.getPrivateKey();
-console.log("privKey:", privKey.toString('HEX'));
- 
-// pubKey
 const pubKey = wallet.getPublicKey();
-console.log("pubKey:", pubKey.toString('HEX'));
-
-// address
 let address = wallet.getAddressString();
+console.log("privKey:", privKey.toString('HEX'));
+console.log("pubKey:", pubKey.toString('HEX'));
 console.log("address:", address);
 
-/***** address *****/
 
-// step 2:  public_key_hash = Keccak-256(public_key)
+// 2
 let public_key_hash = keccak256(pubKey);
-
-// step 3:  address = ‘0x’ + last 20 bytes of public_key_hash
 let address2 = '0x' + public_key_hash.slice(-40)
 console.log(address2);
 
+// 3
 var content = JSON.stringify(wallet.toV3('nccu'));
 fs.writeFile("key.json", content, 'utf8', function(err){
     if(err){
